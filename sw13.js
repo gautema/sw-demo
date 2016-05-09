6e66d43d587986ed8c089c50c14e6115512e3b1b
Gaute Magnussen <gaute.magnussen@gmail.com>
2016-05-09 22:27:32 +0200

    .

--- sw13.js
+++ sw13.js
@@ -1,5 +1,3 @@
-var cache;
-
 self.addEventListener('install', function(event){
   event.waitUntil(self.skipWaiting());
 });
@@ -8,15 +6,12 @@ self.addEventListener('activate', function(event) {
   event.waitUntil(self.clients.claim());
 });
 self.addEventListener('fetch', function(event){
-  console.log(event.request);
-  caches.match(event.request)
+  return caches.match(event.request)
     .then(function(response){
       if(response){
-        console.log(response);
         return response;
       }
       return fetch(event.request).then(function(res){
-        console.log(event.request.url);
         if(event.request.url.endsWith('.jpg')){
           caches.open('horse-app').then(function(cache){
             cache.put(event.request, res.clone());
