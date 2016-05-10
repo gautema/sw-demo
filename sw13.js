d8de498b998bfb99bfbca20c9737ad91c7d6b166
Gaute Magnussen <gaute.magnussen@gmail.com>
2016-05-10 09:13:37 +0200

    Fixed example

--- sw13.js
+++ sw13.js
@@ -5,19 +5,21 @@ self.addEventListener('install', function(event){
 self.addEventListener('activate', function(event) {
   event.waitUntil(self.clients.claim());
 });
+
 self.addEventListener('fetch', function(event){
-  return caches.match(event.request)
+   event.respondWith(caches.match(event.request)
     .then(function(response){
       if(response){
         return response;
       }
       return fetch(event.request).then(function(res){
         if(event.request.url.endsWith('.jpg')){
+          var res2 = res.clone();
           caches.open('horse-app').then(function(cache){
-            cache.put(event.request, res.clone());
+            cache.put(event.request, res2);
           });
         }
         return res;
       });
-    });
+    }));
 });
