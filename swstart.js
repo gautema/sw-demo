e9e89e9323ed030a1b6abb67c0790e1c155a27a6
Gaute Magnussen <gaute.magnussen@gmail.com>
2016-05-12 13:04:11 +0200

    Added empty service worker

---
+++ swstart.js
@@ -0,0 +1,13 @@
+self.addEventListener('install', function(event){
+  event.waitUntil(self.skipWaiting());
+});
+
+self.addEventListener('activate', function(event) {
+  event.waitUntil(self.clients.claim());
+});
+
+self.addEventListener('fetch', function(event){
+   event.respondWith(
+
+  );
+});
